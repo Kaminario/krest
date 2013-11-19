c28690bd2619afe3513495b943f11e8737d78e15
Zaar Hai <haizaar@haizaar.com>
2013-11-19 13:59:37 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='krest',
-      version='0.1',
+      version='0.1.1',
       py_modules=['krest'],
       install_requires=["requests >= 2.0.0"],
       )
