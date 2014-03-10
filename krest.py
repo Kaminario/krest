1a04df412ab1de0d6c2267a85a5c8cb3f8284d0b
Zaar Hai <haizaar@haizaar.com>
2014-03-10 10:12:39 +0200

    version bump to 1.1.0

--- krest.py
+++ krest.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-__version__ = "1.0.10"
+__version__ = "1.1.0"
 
 import json
 import urlparse
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="krest",
-      version="1.0.10",
+      version="1.1.0",
       py_modules=["krest"],
       install_requires=["requests >= 2.0.0"],
       )
