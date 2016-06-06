c93b30a034b1cd65f13effbbb568fd539a9deec0
Zaar Hai <haizaar@users.noreply.github.com>
2016-06-06 16:07:13 +0300

    Updated classifiers for Python 3

--- setup.py
+++ setup.py
@@ -37,5 +37,6 @@ setup(name="krest",
                    'License :: OSI Approved :: Apache Software License',
                    'Programming Language :: Python',
                    'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7'),
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.4'),
       )
